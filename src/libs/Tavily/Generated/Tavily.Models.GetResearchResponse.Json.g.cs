66d4642e022ea07d898ca33edf32610bff05d289
Konstantin S. <havendv@gmail.com>
2026-07-30 20:21:16 +0400

    feat: Updated OpenAPI spec (#132)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Tavily/Generated/Tavily.Models.GetResearchResponse.Json.g.cs
+++ src/libs/Tavily/Generated/Tavily.Models.GetResearchResponse.Json.g.cs
@@ -2,7 +2,7 @@
 
 namespace Tavily
 {
-    public readonly partial struct GetResearchResponse
+    public sealed partial class GetResearchResponse
     {
         /// <summary>
         /// Serializes the current instance to a JSON string using the provided JsonSerializerContext.
@@ -54,7 +54,7 @@ namespace Tavily
             return global::System.Text.Json.JsonSerializer.Deserialize(
                 json,
                 typeof(global::Tavily.GetResearchResponse),
-                jsonSerializerContext) as global::Tavily.GetResearchResponse?;
+                jsonSerializerContext) as global::Tavily.GetResearchResponse;
         }
 
         /// <summary>
@@ -101,7 +101,7 @@ namespace Tavily
             return (await global::System.Text.Json.JsonSerializer.DeserializeAsync(
                 jsonStream,
                 typeof(global::Tavily.GetResearchResponse),
-                jsonSerializerContext).ConfigureAwait(false)) as global::Tavily.GetResearchResponse?;
+                jsonSerializerContext).ConfigureAwait(false)) as global::Tavily.GetResearchResponse;
         }
 
         /// <summary>

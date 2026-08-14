8b7ce66c9e699c59f3cb6b319250a001427abbe2
Konstantin S. <havendv@gmail.com>
2026-08-14 19:42:54 +0400

    feat: Updated OpenAPI spec (#140)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Tavily/Generated/Tavily.TavilyClient.g.cs
+++ src/libs/Tavily/Generated/Tavily.TavilyClient.g.cs
@@ -34,10 +34,17 @@ namespace Tavily
 
         /// <inheritdoc/>
         public global::Tavily.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Tavily.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Tavily.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
